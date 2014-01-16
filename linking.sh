ef3a148f563b1e6ca64ea08bfc668eebb30cb3b7
Jesse Ahrens <sprack@gmail.com>
2014-01-16 14:21:20 -0800

    condense OSX paths for setting up linked configs

--- linking.sh
+++ linking.sh
@@ -1,7 +1,7 @@
 # OSX
 rm ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/Color\ Scheme\ -\ Default/All\ Hallow\'s\ Eve.tmTheme
-ln -s ~/git/working/sublime_settings/All\ Hallow\'s\ Eve.tmTheme ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/Color\ Scheme\ -\ Default/All\ Hallow\'s\ Eve.tmTheme
+ln -s ~/git/sublime_settings/All\ Hallow\'s\ Eve.tmTheme ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/Color\ Scheme\ -\ Default/All\ Hallow\'s\ Eve.tmTheme
 
 rm -rf ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User
-ln -s ~/git/working/sublime_settings/User ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User
+ln -s ~/git/sublime_settings/User ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User
 
