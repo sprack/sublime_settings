8e6f794d70f2f7ec7a3eb430f5fe09bf829636cc
Jesse Ahrens <sprack@gmail.com>
2014-02-04 11:29:19 -0800

    add keymap for datestamp, change replace to insert on empty

--- User/insert_datetime.py
+++ User/insert_datetime.py
@@ -2,6 +2,10 @@ import sublime, sublime_plugin, time
 
 class InsertDatetimeCommand(sublime_plugin.TextCommand):
     def run(self, edit):
+	format = "%Y-%m-%d %H.%M:%S"
         sel = self.view.sel();
         for s in sel:
-            self.view.replace(edit, s, time.ctime())
+	    if s.empty():
+                self.view.insert(edit, s.a, strftime(format, localtime()))
+	    else:
+                self.view.replace(edit, s, strftime(format, localtime()))
